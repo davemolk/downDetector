9e4b6d5db559c36d3c5403577ada5537ada19716
davemolk <davemolk@gmail.com>
2022-05-19 08:46:29 -0600

    user agents

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"time"
 )
@@ -12,7 +13,7 @@ type Job struct {
 	URL string
 }
 
-func worker(id int, jobs <-chan Job, results chan<-int, client *http.Client){
+func worker(id int, jobs <-chan Job, results chan<-int, client *http.Client, ua string){
 	for j := range jobs{
 		req, err := http.NewRequest("GET", j.URL, nil)
 		log.Println("id, url:", id, j.URL)
@@ -20,6 +21,8 @@ func worker(id int, jobs <-chan Job, results chan<-int, client *http.Client){
 			log.Fatal("Unable to create request", err)
 		}
 		
+		req.Header.Set("User-Agent", ua)
+
 		res, err := client.Do(req)
 		if err != nil {
 			log.Println("request unsuccessful so site probably down", id, err)
@@ -35,6 +38,32 @@ func worker(id int, jobs <-chan Job, results chan<-int, client *http.Client){
 			results <- 1
 		}
 	}
+}
+
+func randomUA() []string {
+	userAgents := []string{
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko)",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36",
+	}
+
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	for i := 0; i < len(userAgents); i++ {
+		j := r.Intn(len(userAgents))
+
+		ua1 := userAgents[i]
+		ua2 := userAgents[j]
+		userAgents[i] = ua2
+		userAgents[j] = ua1
+	}
+
+	return userAgents
 }
 
 func main() {
@@ -53,8 +82,10 @@ func main() {
 	jobs := make(chan Job, *attempts)
 	results := make(chan int, *attempts)
 
+	userAgents := randomUA()
+
 	for w := 1; w <= 3; w++ {
-		go worker(w, jobs, results, client)
+		go worker(w, jobs, results, client, userAgents[w-1])
 	}
 
 	for j := 1; j <= *attempts; j++ {
